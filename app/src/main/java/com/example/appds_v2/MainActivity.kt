f97e88fb7e176b593557657a4090afed7039a38a
hienkcxd <70784911+hienkcxd@users.noreply.github.com>
2023-03-28 10:51:58 +0700

    hide ban phim khi an vao screen

--- app/src/main/java/com/example/appds_v2/MainActivity.kt
+++ app/src/main/java/com/example/appds_v2/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.appds_v2
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.inputmethod.InputMethodManager
+import com.example.appds_v2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
+        return super.onTouchEvent(event)
     }
 }
